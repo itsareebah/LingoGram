dda5d751932690aef18a2b6c135832a10150e322
itsraeebah <areebahsuhail4@gmail.com>
2024-03-24 19:41:39 +0530

    'update'

--- commands/commandsList.py
+++
@@ -1,6 +0,0 @@
-# import linguaCommand
-
-# def commands_list():
-#     commands = []
-#     commands.append(linguaCommand.data)
-#     return commands
--- src/process.py
+++ src/process.py
@@ -1,6 +1,3 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-OPEN_API_KEY = "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"
+OPEN_API_KEY = os.environ.get('OPEN_API_KEY')
